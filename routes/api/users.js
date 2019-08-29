4d6f59f537c0ddd3cd91809c23410ee5d9a1daff
Sam Odum <sam.odum1@gmail.com>
2019-08-29 23:49:35 +0100

    Prepped users.js for handling ruser registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 
+const User = require("../../models/User");
+
 // Route    POST api/users
 // Desc     Register user
 // Access   Public
@@ -22,6 +24,16 @@ router.post(
     if (!error.isEmpty()) {
       return res.status(400).json({ error: error.array() });
     }
+
+    const { name, email, password } = req.body;
+
+    // TO DOs
+
+    // **Confirm that user exists
+    // **Fetch user's image
+    // **Encrypt user's password
+    // **Return jsonwebtoken
+
     res.send("User route");
   }
 );
