a763bcca2ae76ec55971905a82257930e60707d1
Sam Odum <sam.odum1@gmail.com>
2019-10-17 23:12:29 +0100

    Created file to fetch user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
-
-// Route    GET api/profile
-// Desc     Test Route
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+// Route    GET api/profile/me
+// Desc     Get profile of current user
 // Access   Public
-router.get('/', (req, res)=> res.send('Profile route'));
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate("user", ["name", "avater"]);
+
+        if (!profile) {
+            return res.status(400).json({ msg: "This user has no profile" })
+        }
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+});
 
 module.exports = router;
